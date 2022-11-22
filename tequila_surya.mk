a6842898256cfeed833f7b47aad565035b224adf
TheStrechh <TheStrechh@gmail.com>
2022-11-22 19:28:03 +0000

    surya: Tequila time

--- tequila_surya.mk
+++ tequila_surya.mk
@@ -21,16 +21,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TequilaOS stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := tequila_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Blur
-TARGET_ENABLE_BLUR := true
